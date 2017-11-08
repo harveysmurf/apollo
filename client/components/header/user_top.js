df576a62910e08530b6be1a7464139e4448f88c3
Simeon Babev <harveysmurf@abv.bg>
2017-11-08 20:02:52 +0200

    Create dropdown component

--- client/components/header/user_top.js
+++ client/components/header/user_top.js
@@ -3,14 +3,7 @@ import {graphql } from 'react-apollo';
 import gql from 'graphql-tag'
 import { Link} from 'react-router-dom'
 import { withRouter } from 'react-router'
-
-const styles = {
-    marginRight(amount) {
-        return {
-            marginRight: amount
-        }
-    }
-}
+import Dropdown from '../utilities/dropdown'
 
 
 const withLoggedInUserData = graphql(gql`query { loggedInUser { name } }`);
@@ -22,26 +15,44 @@ class UserTop extends Component {
         const user = this.props.data.loggedInUser
             return (
                 <div>
+                    <Dropdown className="user-link" button_content={
+                            <div>
+                                <i className="fa fa-phone" aria-hidden="true"></i>
+                                <span>
+                                За връзка
+                                </span>
+                            </div>
+                            }
+                            dropdown_data={
+                                <div>
+                                <div>0987 23 34 32</div>
+                                <div>2323 34 32 23</div>
+                                </div>
+                            }
+                    />
                     {!user &&
-                        <Link to="/register" style={styles.marginRight(10)}>Регистрация</Link>
+                        <Link to="/register" className="user-link" >
+                        <i class="fa fa-user-plus" aria-hidden="true"></i>
+                        Регистрация
+                        </Link>
                     }
                     {user &&
-                    <a href="#" className="dropdown" style={styles.marginRight(10)}>
-                        <i className="fa fa-user" aria-hidden="true" style={styles.marginRight(3)}></i>
-                        <span style={styles.marginRight(3)}>
+                    <a href="#" className="dropdown" className="user-link" >
+                        <i className="fa fa-user" aria-hidden="true"></i>
+                        <span >
                         Профил
                         </span>
                     </a>
                     }
 
                     {user && 
-                    <a href="#" className="favorites" style={styles.marginRight(10)}>
-                        <i className="fa fa-heart" aria-hidden="true" style={styles.marginRight(3)}></i>
+                    <a href="#" className="favorites" className="user-link">
+                        <i className="fa fa-heart" aria-hidden="true" ></i>
                         Любими
                     </a>
                     }
                     <a href="#" className="top-cart">
-                        <i className="fa fa-shopping-cart" aria-hidden="true" style={styles.marginRight(3)}></i>
+                        <i className="fa fa-shopping-cart" aria-hidden="true" ></i>
                         Количка
                     </a>
                 </div>
---
+++ client/components/utilities/dropdown.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+
+export default class Dropdown extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: false
+        }
+        this.toggle = this.toggle.bind(this)
+    }
+    toggle() {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+    render() {
+        let status = this.state.open
+        let button_content = this.props.button_content
+        let dropdown_data = this.props.dropdown_data
+        let dropdownClass = status ? 'open' : ''
+        let className = 'dropdown-toggle ' + this.props.className || ''
+        return (
+
+        <a onClick={this.toggle} href="#" className={className}>
+            {button_content}
+            <div className={'dropdown ' + dropdownClass}>
+                {dropdown_data}
+            </div>
+        </a>
+        )
+    }
+}
+
