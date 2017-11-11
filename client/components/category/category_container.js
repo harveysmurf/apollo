e369e3e73458ac841928f276598b6a0af5853917
Simeon Babev <harveysmurf@abv.bg>
2017-11-11 22:35:26 +0200

    Fix category_container

--- client/components/category/category_container.js
+++ client/components/category/category_container.js
@@ -5,17 +5,27 @@ import gql from 'graphql-tag'
 const query = gql`
 query getCategoryQuery($slug: String) {
     getCategory(slug: $slug) {
-        name
+        name,
+        products {
+            name
+        }
+
     }
 }
 `
 
-const CategoryContainer = (props) => {
-    return (
-    <div className="category">
-        <h3>Category</h3>
-    </div>
-    )
+const CategoryContainer = ({data}) => {
+    if(data.loading)
+        return (<div>Loading</div>)
+    else
+        return (
+        <div className="category">
+            <h3>Category</h3>
+            {data.getCategory.products.map((product) => {
+                return <span>{product.name}</span>
+            })}
+        </div>
+        )
 }
 
 export default graphql(query, {
